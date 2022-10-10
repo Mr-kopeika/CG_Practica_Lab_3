314f6bcc231b30b619950d44092c4c80b0217f39
Mr-kopeika <102347082+Mr-kopeika@users.noreply.github.com>
2022-10-10 08:45:43 +0700

    Добавьте файлы проекта.

---
+++ CG_Practica_Lab_3/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace CG_Practica_Lab_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DrawButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // DrawButton
+            // 
+            this.DrawButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.DrawButton.Location = new System.Drawing.Point(13, 526);
+            this.DrawButton.Name = "DrawButton";
+            this.DrawButton.Size = new System.Drawing.Size(145, 23);
+            this.DrawButton.TabIndex = 0;
+            this.DrawButton.Text = "Построить фигуру";
+            this.DrawButton.UseVisualStyleBackColor = true;
+            this.DrawButton.Click += new System.EventHandler(this.DrawButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(177, 525);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Отразить по вертикали";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Vertical_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.Location = new System.Drawing.Point(177, 496);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(145, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Отразить по горизонтали";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Horizontal_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button4.Location = new System.Drawing.Point(352, 525);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(145, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Сдвиг";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Shift_Click);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button5.Location = new System.Drawing.Point(534, 525);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(144, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Масштаб";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Scale_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(352, 506);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "X:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(375, 503);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(45, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(427, 506);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Y:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox2.Location = new System.Drawing.Point(452, 503);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(45, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "0";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox3.Location = new System.Drawing.Point(631, 503);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(45, 20);
+            this.textBox3.TabIndex = 12;
+            this.textBox3.Text = "1";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(606, 506);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Y:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox4.Location = new System.Drawing.Point(554, 503);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(45, 20);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Text = "1";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(531, 506);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "X:";
+            // 
+            // button6
+            // 
+            this.button6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button6.Location = new System.Drawing.Point(710, 525);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(142, 23);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "Поворот";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Rotate_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox5.Location = new System.Drawing.Point(710, 503);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(142, 20);
+            this.textBox5.TabIndex = 14;
+            this.textBox5.Text = "45";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.DrawButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.DrawButton_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DrawButton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ CG_Practica_Lab_3/Form1.cs
@@ -0,0 +1,414 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CG_Practica_Lab_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Graphics g;
+        Point[] points;
+        Point center;
+        Pen pen;
+        int W;
+        int H;
+        int step;
+        int numPoint;
+
+
+
+        public Point GetCenter()
+        {
+            
+            int Xleft = 0, Xright = 0, Yup = 0, Ydown = 0;
+
+            for(int i = 0; i < points.Length; i++)
+            {
+                if (points[i].X < Xleft || i == 0) Xleft = points[i].X;
+                if (points[i].X > Xright || i == 0) Xright = points[i].X;
+                if (points[i].Y > Yup || i == 0) Yup = points[i].Y;
+                if (points[i].Y < Ydown || i == 0) Ydown = points[i].Y;
+            }
+            center = new Point(Xleft/2 + Xright/2, Yup/2 + Ydown/2);
+            //можно улучшить, а можно оставить
+            return center;
+        }
+
+        public void DrawFigure()
+        {
+            pen.Width = 3;
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                if (i + 1 != points.Length)
+                    g.DrawLine(pen, points[i], points[i + 1]);
+                else g.DrawLine(pen, points[i], points[0]);
+            }
+            g.DrawRectangle(pen, center.X, center.Y, 3, 3);
+        }
+
+        public void DrawAxis()
+        {
+
+            Refresh();
+            g = this.CreateGraphics();
+            pen = new Pen(Color.FromArgb(255, 0, 0, 0));
+            W = ClientSize.Width;
+            H = ClientSize.Height;
+            step = 50;
+            numPoint = 5;
+
+            //координатные оси
+            g.DrawLine(pen, W / 2, 0, W / 2, H);
+            g.DrawLine(pen, 0, H / 2, W, H / 2);
+
+            //стрелочки
+            g.DrawLine(pen, W / 2, 0, W / 2 - 10, 15);
+            g.DrawLine(pen, W / 2, 0, W / 2 + 10, 15);
+            g.DrawLine(pen, W, H / 2, W - 15, H / 2 - 10);
+            g.DrawLine(pen, W, H / 2, W - 15, H / 2 + 10);
+
+            //насечки с цифрами на координатных осях 
+            int x = 0, y = 0, text = 0;
+            x = W / 2; y = H / 2;
+            while (x < ClientSize.Width)
+            {
+                g.DrawLine(pen, x, y - 5, x, y + 5);
+                if (x != W / 2) g.DrawString(text.ToString(), new Font("Tahoma", 7), Brushes.Black, x - 4, y + 7);
+                text += 1;
+                x += 50;
+            }
+            x = W / 2; text = 0;
+            while (x > 0)
+            {
+                g.DrawLine(pen, x, y - 5, x, y + 5);
+                if (x != W / 2) g.DrawString(text.ToString(), new Font("Tahoma", 7), Brushes.Black, x - 8, y + 7);
+                text -= 1;
+                x -= 50;
+            }
+            x = W / 2; text = 0;
+            while (y < H)
+            {
+                g.DrawLine(pen, x - 5, y, x + 5, y);
+                if (y != H / 2) g.DrawString(text.ToString(), new Font("Tahoma", 7), Brushes.Black, x + 7, y - 5);
+                text -= 1;
+                y += 50;
+            }
+            y = H / 2; text = 0;
+            while (y > 0)
+            {
+                g.DrawLine(pen, x - 5, y, x + 5, y);
+                if (y != H / 2) g.DrawString(text.ToString(), new Font("Tahoma", 7), Brushes.Black, x + 7, y - 5);
+                y -= 50;
+                text += 1;
+            }
+        }
+
+        public int[ , ] ToMatrix(Point[] points)
+        {
+            int[,] ints = new int[points.Length, 3];
+
+            for(int i = 0; i < ints.GetLength(0); i++)
+            {
+                ints[i, 0] = points[i].X;
+                ints[i, 1] = points[i].Y;
+                ints[i, 2] = 1;
+            }
+
+            return ints;
+        }
+
+        public Point[] ToPoints(int[,] ints)
+        {
+            Point[] points = new Point[ints.Length / 3];
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i] = new Point(ints[i, 0], ints[i, 1]);
+            }
+
+            return points;
+        }
+
+        public Point[] ShiftPoints(Point[] points, int X, int Y)
+        {
+            int[,] ints = ToMatrix(points);
+
+            int[,] shiftMatrix = new int[3, 3] { 
+                { 1, 0, 0}, 
+                { 0, 1, 0}, 
+                { X, Y, 1} 
+            };
+
+            ints = MultyplicationMatrix(ints, shiftMatrix);
+
+            points = ToPoints(ints);
+            return points;
+        }
+
+        public Point[] ScaleMatrix(Point[] points, double X, double Y)
+        {
+            int[,] ints = ToMatrix(points);
+
+            double[,] shiftMatrix = new double[3, 3] {
+                { X, 0, 0},
+                { 0, Y, 0},
+                { 0, 0, 1}
+            };
+
+            ints = MultyplicationMatrix(ints, shiftMatrix);
+
+            points = ToPoints(ints);
+            return points;
+        }
+
+        public int[,] MultyplicationMatrix(int[,] matrix1, int[,] matrix2)
+        {
+            if (matrix1.GetLength(1) != matrix2.GetLength(0)) return new int[,] { { 0, 0 }, { 0, 0} };
+            int[,] resultMatrix = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+            for (int i = 0; i < matrix1.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix2.GetLength(1); j++)
+                {
+                    int temp = 0;
+                    for (int k = 0; k < matrix1.GetLength(1); k++)
+                    {
+                        temp += matrix1[i, k] * matrix2[k, j];
+                    }
+                    resultMatrix[i, j] = temp;
+                }
+            }
+
+            return resultMatrix;
+        }
+
+        public Point[] HorizontalReflection()
+        {
+            int[,] ints = ToMatrix(points);
+
+            double[,] shiftMatrix = new double[3, 3] {
+                { 1, 0, 0},
+                { 0, -1, 0},
+                { 0, 0, 1}
+            };
+
+            ints = MultyplicationMatrix(ints, shiftMatrix);
+
+            points = ToPoints(ints);
+            return points;
+        }
+
+        public Point[] VerticalReflection()
+        {
+            int[,] ints = ToMatrix(points);
+
+            double[,] shiftMatrix = new double[3, 3] {
+                { -1, 0, 0},
+                { 0, 1, 0},
+                { 0, 0, 1}
+            };
+
+            ints = MultyplicationMatrix(ints, shiftMatrix);
+
+            points = ToPoints(ints);
+            return points;
+        }
+
+        public int[,] MultyplicationMatrix(int[,] matrix1, double[,] matrix2)
+        {
+            if (matrix1.GetLength(1) != matrix2.GetLength(0)) return new int[,] { { 0, 0 }, { 0, 0 } };
+            int[,] resultMatrix = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+            for (int i = 0; i < matrix1.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix2.GetLength(1); j++)
+                {
+                    double temp = 0;
+                    for (int k = 0; k < matrix1.GetLength(1); k++)
+                    {
+                        temp += matrix1[i, k] * matrix2[k, j];
+                    }
+                    resultMatrix[i, j] = (int)temp;
+                }
+            }
+
+            return resultMatrix;
+        }
+
+        public Point[] RotatePoints(Point[] points, int degree)
+        {
+            int[,] ints = ToMatrix(points);
+
+            double[,] shiftMatrix = new double[3, 3] {
+                { Math.Cos(degree * Math.PI / 180), Math.Sin(degree * Math.PI / 180), 0},
+                { -Math.Sin(degree * Math.PI / 180), Math.Cos(degree * Math.PI / 180), 0},
+                { 0, 0, 1}
+            };
+
+            ints = MultyplicationMatrix(ints, shiftMatrix);
+
+            points = ToPoints(ints);
+
+            return points;
+        }
+
+        private void DrawButton_Click(object sender, EventArgs e)
+        {
+
+            {
+                Refresh();
+                g = this.CreateGraphics();
+                pen = new Pen(Color.FromArgb(255, 0, 0, 0));
+                W = ClientSize.Width;
+                H = ClientSize.Height;
+                step = 50;
+                numPoint = 5;
+
+                //координатные оси
+                g.DrawLine(pen, W / 2, 0, W / 2, H);
+                g.DrawLine(pen, 0, H / 2, W, H / 2);
+
+                //стрелочки
+                g.DrawLine(pen, W / 2, 0, W / 2 - 10, 15);
+                g.DrawLine(pen, W / 2, 0, W / 2 + 10, 15);
+                g.DrawLine(pen, W, H / 2, W - 15, H / 2 - 10);
+                g.DrawLine(pen, W, H / 2, W - 15, H / 2 + 10);
+
+                //насечки с цифрами на координатных осях 
+                int x = 0, y = 0, text = 0;
+                x = W / 2; y = H / 2;
+                while (x < ClientSize.Width)
+                {
+                    g.DrawLine(pen, x, y - 5, x, y + 5);
+                    if (x != W / 2) g.DrawString(text.ToString(), new Font("Tahoma", 7), Brushes.Black, x - 4, y + 7);
+                    text += 1;
+                    x += 50;
+                }
+                x = W / 2; text = 0;
+                while (x > 0)
+                {
+                    g.DrawLine(pen, x, y - 5, x, y + 5);
+                    if (x != W / 2) g.DrawString(text.ToString(), new Font("Tahoma", 7), Brushes.Black, x - 8, y + 7);
+                    text -= 1;
+                    x -= 50;
+                }
+                x = W / 2; text = 0;
+                while (y < H)
+                {
+                    g.DrawLine(pen, x - 5, y, x + 5, y);
+                    if (y != H / 2) g.DrawString(text.ToString(), new Font("Tahoma", 7), Brushes.Black, x + 7, y - 5);
+                    text -= 1;
+                    y += 50;
+                }
+                y = H / 2; text = 0;
+                while (y > 0)
+                {
+                    g.DrawLine(pen, x - 5, y, x + 5, y);
+                    if (y != H / 2) g.DrawString(text.ToString(), new Font("Tahoma", 7), Brushes.Black, x + 7, y - 5);
+                    y -= 50;
+                    text += 1;
+                }
+
+                pen.Width = 3;
+                points = new Point[numPoint];
+                points[0] = new Point((W / 2) - 2 * step, H / 2 - 4 * step);
+                points[1] = new Point(W / 2 + 2 * step, H / 2 - 2 * step);
+                points[2] = new Point(W / 2 - step, H / 2 + 2 * step);
+                points[3] = new Point(W / 2 - step, H / 2);
+                points[4] = new Point(W / 2 - 3 * step, H / 2 + 2 * step);
+                //Фигура
+                for (int i = 0; i < points.Length; i++)
+                {
+                    if (i + 1 != points.Length)
+                        g.DrawLine(pen, points[i], points[i + 1]);
+                    else g.DrawLine(pen, points[i], points[0]);
+                }
+
+                Point center = GetCenter();
+                g.DrawRectangle(pen, center.X, center.Y, 3, 3);
+                g.DrawLine(pen, W / 2 + center.X * step, W / 2 + center.X * step, H / 2 + center.Y * step, H / 2 + center.Y * step);
+
+                g.Dispose();
+            }
+        }
+
+        private void Horizontal_Click(object sender, EventArgs e)
+        {
+
+            {
+                points = ShiftPoints(points, -W / 2, -H / 2);
+                points = HorizontalReflection();
+                points = ShiftPoints(points, W / 2, H / 2);
+                DrawAxis();
+                DrawFigure();
+            }
+        }
+
+        private void Vertical_Click(object sender, EventArgs e)
+        {
+
+            {
+                points = ShiftPoints(points, -W / 2, -H / 2);
+                points = VerticalReflection();
+                points = ShiftPoints(points, W / 2, H / 2);
+                DrawAxis();
+                DrawFigure();
+            }
+        }
+
+        private void Shift_Click(object sender, EventArgs e)
+        {
+
+            {
+                int X = int.Parse(textBox1.Text);
+                int Y = int.Parse(textBox2.Text);
+
+                points = ShiftPoints(points, X, Y);
+                GetCenter();
+                DrawAxis();
+                DrawFigure();
+            }
+        }
+
+        private void Scale_Click(object sender, EventArgs e)
+        {
+
+            {
+                double X = Double.Parse(textBox3.Text);
+                double Y = Double.Parse(textBox4.Text);
+
+                points = ShiftPoints(points, -center.X, -center.Y);
+                points = ScaleMatrix(points, X, Y);
+                points = ShiftPoints(points, center.X, center.Y);
+                DrawAxis();
+                DrawFigure();
+            }
+        }
+
+        private void Rotate_Click(object sender, EventArgs e)
+        {
+
+            {
+                int degree = int.Parse(textBox5.Text);
+                points = ShiftPoints(points, -center.X, -center.Y);
+                points = RotatePoints(points, degree);
+                points = ShiftPoints(points, center.X, center.Y);
+                DrawAxis();
+                DrawFigure();
+            }
+        }
+    }
+}
